fbd766267467c3030093b4d66bebd9d444c0548f
Zetka <jakub.kochanski10@gmail.com>
2021-11-23 23:26:57 +0100

    Dodanie zdjęcia w  tle

---
+++ pomodoro_app/lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+import 'home.dart';
+import 'settings.dart';
+
+void main() {
+  runApp(MaterialApp(
+    initialRoute: '/',
+    routes: {
+      '/': (context) => Home(),
+      // '/settings':(context)=>Settings(),
+    },
+  ));
+}
