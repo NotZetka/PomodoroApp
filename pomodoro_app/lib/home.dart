d7576cd5b2e3acc164743d6549070f7242aa329e
Zetka <jakub.kochanski10@gmail.com>
2021-11-24 11:29:00 +0100

    Ustawienie przycisku ustawień na miejscu

--- pomodoro_app/lib/home.dart
+++ pomodoro_app/lib/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
@@ -11,11 +12,55 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        decoration: BoxDecoration(
-          image: DecorationImage(
-              image: AssetImage("assets/bgimage.png"), fit: BoxFit.cover),
-        ),
+      backgroundColor: Colors.pink[400],
+      body: SafeArea(
+        child: Container(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                  image: AssetImage("assets/bgimage.png"), fit: BoxFit.cover),
+            ),
+            child: Center(
+              //layoutbuilder is responsive for sizing app
+              child: LayoutBuilder(builder: (context, constraints) {
+                return Column(
+                  children: [
+                    //Settings button
+                    Container(
+                      height: constraints.maxHeight * 0.08,
+                      child: Align(
+                        child: Padding(
+                          padding: const EdgeInsets.fromLTRB(0, 0, 20, 0),
+                          child: IconButton(
+                            onPressed: () {},
+                            icon: Icon(
+                              Icons.settings,
+                              color: Colors.white,
+                            ),
+                            alignment: Alignment.centerRight,
+                          ),
+                        ),
+                        alignment: Alignment.centerRight,
+                      ),
+                    ),
+                    Expanded(
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: IconButton(
+                          onPressed: () {},
+                          icon: Icon(
+                            Icons.settings,
+                          ),
+                        ),
+                      ),
+                    ),
+                    Container(
+                      height: constraints.maxHeight * 0.08,
+                      child: SizedBox(),
+                    )
+                  ],
+                );
+              }),
+            )),
       ),
     );
   }
