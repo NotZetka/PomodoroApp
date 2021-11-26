0eadd158056e86304f5c819778c4ea44ba3c0006
Zetka <jakub.kochanski10@gmail.com>
2021-11-26 12:54:39 +0100

    Added timer not finished

--- pomodoro_app/lib/home.dart
+++ pomodoro_app/lib/home.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
+import 'package:circular_countdown_timer/circular_countdown_timer.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
@@ -9,59 +11,82 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  CountDownController _controller = CountDownController();
+  int duration = 100;
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.pink[400],
-      body: SafeArea(
-        child: Container(
-            decoration: BoxDecoration(
-              image: DecorationImage(
-                  image: AssetImage("assets/bgimage.png"), fit: BoxFit.cover),
+    return Stack(children: [
+      Image.asset('assets/bgimage.png', fit: BoxFit.cover),
+      Scaffold(
+        backgroundColor: Colors.transparent,
+        appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          elevation: 0,
+          actions: [
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: IconButton(
+                onPressed: () {},
+                icon: Icon(Icons.settings),
+              ),
             ),
-            child: Center(
-              //layoutbuilder is responsive for sizing app
-              child: LayoutBuilder(builder: (context, constraints) {
-                return Column(
-                  children: [
-                    //Settings button
-                    Container(
-                      height: constraints.maxHeight * 0.08,
-                      child: Align(
+          ],
+        ),
+        body: SafeArea(child: Center(
+          //layoutbuilder is responsive for sizing app
+          child: LayoutBuilder(builder: (context, constraints) {
+            return Column(
+              children: [
+                //Settings button
+                Expanded(
+                  child: Row(
+                    children: [
+                      Expanded(
+                        child: SizedBox(),
+                        flex: 5,
+                      ),
+                      Expanded(
+                        flex: 5,
                         child: Padding(
-                          padding: const EdgeInsets.fromLTRB(0, 0, 20, 0),
-                          child: IconButton(
-                            onPressed: () {},
-                            icon: Icon(
-                              Icons.settings,
-                              color: Colors.white,
+                          padding: const EdgeInsets.all(8.0),
+                          child: Opacity(
+                            opacity: 0.6,
+                            child: CircularCountDownTimer(
+                              width: constraints.maxWidth * 0.25,
+                              height: constraints.maxHeight * 0.2,
+                              duration: duration,
+                              fillColor: Colors.pink,
+                              ringColor: Colors.purple,
+                              strokeWidth: 10.0,
+                              textStyle: TextStyle(
+                                fontSize: 23.0,
+                                color: Colors.white,
+                                fontFamily: 'Montserrat',
+                              ),
+                              isReverse: true,
+                              autoStart: false,
                             ),
-                            alignment: Alignment.centerRight,
-                          ),
-                        ),
-                        alignment: Alignment.centerRight,
-                      ),
-                    ),
-                    Expanded(
-                      child: Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: IconButton(
-                          onPressed: () {},
-                          icon: Icon(
-                            Icons.settings,
                           ),
                         ),
                       ),
-                    ),
-                    Container(
-                      height: constraints.maxHeight * 0.08,
-                      child: SizedBox(),
-                    )
-                  ],
-                );
-              }),
-            )),
+                      Expanded(
+                        child: SizedBox(),
+                        flex: 5,
+                      )
+                    ],
+                  ),
+                ),
+
+                Container(
+                  height: constraints.maxHeight * 0.2,
+                  child: SizedBox(),
+                )
+              ],
+            );
+          }),
+        )),
       ),
-    );
+    ]);
   }
 }
